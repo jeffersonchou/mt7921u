49b88c2971fa8523133f48c8a0ed7c15f55a3469
jeff <chou.jefferson@me.com>
2024-12-13 22:11:13 +0800

    initial commit

---
+++ export_android.sh
@@ -0,0 +1,7 @@
+export AOSP_TOPDIR=/home/jeff/Downloads/vim3-aosp-12r127
+export PATH=${AOSP_TOPDIR}/prebuilts/clang/host/linux-x86/clang-r416183b/bin:$PATH
+export PATH=${AOSP_TOPDIR}/prebuilts/gas/linux-x86:$PATH
+export PATH=${AOSP_TOPDIR}/prebuilts/misc/linux-x86/lz4:$PATH
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+export LLVM=1
